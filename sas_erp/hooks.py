3f82b1e7706c1a2ee00a2cbf4d8557d051b01431
irfan <irfan@sastechnologies.co>
2026-09-21 08:51:19 +0200

    translator remove, already availble in sascustom

--- sas_erp/hooks.py
+++ sas_erp/hooks.py
@@ -39,11 +39,11 @@ app_license = "mit"
 # webform_include_js = {"doctype": "public/js/doctype.js"}
 # webform_include_css = {"doctype": "public/css/doctype.css"}
 
-jinja = {
-       "methods": [
-               "sas_erp.sas_erp.custom_script.translator.translate_text"
-       ]
-}
+# jinja = {
+#        "methods": [
+
+#        ]
+# }
 
 # include js in page
 # page_js = {"page" : "public/js/file.js"}
--- sas_erp/sas_erp/custom_script/translator.py
+++
@@ -1,6 +0,0 @@
-from deep_translator import GoogleTranslator
-
-def translate_text(text, target_language='en'):
-    translator = GoogleTranslator(target=target_language)
-    translated_text = translator.translate(text)
-    return translated_text
